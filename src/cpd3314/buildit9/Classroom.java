cdbac2ba023a842c43f14dbf4fc65f9c4becd544
Mark Russell <mark.russell@lambtoncollege.ca>
2015-03-09 14:58:34 -0400

    w2015 Update

--- src/cpd3314/buildit9/Classroom.java
+++ src/cpd3314/buildit9/Classroom.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2014 Len Payne <len.payne@lambtoncollege.ca>.
+ * Updated 2015 Mark Russell <mark.russell@lambtoncollege.ca>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package cpd3314.buildit9;
 
@@ -9,7 +20,7 @@ import java.util.Arrays;
 
 /**
  *
- * @author c0587637
+ * @author c0587637 Len Payne <len.payne@lambtoncollege.ca>
  */
 public class Classroom {
 
@@ -35,6 +46,7 @@ public class Classroom {
         }
         newStudents[i] = s;
         students = newStudents;
+        
         // Short Way
         // students = Arrays.copyOf(students, students.length + 1);
         // students[students.length - 1] = s;        
